c2b190b6539872219c578e9eb0af9a77a83cacb9
Saadet Simsek <saadetsimsek13@gmail.com>
2024-01-09 14:04:59 +0300

    Initial Commit

---
+++ StocksAndBanking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksAndBanking
+//
+//  Created by Saadet Şimşek on 09/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
